3e4059caf197d0b822bc4ee1b1bb16092e0c16aa
Franck <fonkouakamdemf@gmail.com>
2026-04-14 05:21:11 +0200

    add of external functions

--- homework_4/include/homework_4.h
+++ homework_4/include/homework_4.h
@@ -2,27 +2,42 @@
 #define HOMEWORK_4_H
 
 #include <algorithm>
-#include <iostream>
+#include <string>
 #include <vector>
-using std::string;
-using std::vector;
 
 namespace ipb {
-template <typename T> struct named_vector {
-  string name_;
-  vector<T> data_;
+template <typename T>
+
+struct named_vector {
+  std::string name_;
+  std::vector<T> data_;
+  // ✅ CONSTRUCTEUR (fonction spéciale)
+  named_vector() : name_(""), data_() {}
+
+  // ✅ CONSTRUCTEUR (fonction spéciale)
+  named_vector(const std::string &n, const std::vector<T> &d)
+      : name_(n), data_(d) {}
+
   std::string name() const { return name_; }
-  vector<T> data() const { return data_; }
+  std::vector<T> &vector() { return data_; }
+
+  void push_back(const T &value) { data_.push_back(value); }
   void emplace_back(const T &value) { data_.emplace_back(value); }
+
   T &operator[](size_t i) { return data_[i]; }
-  T &begin() { return data_.front(); }
-  T &end() { return data_.back(); }
+
+  auto begin() { return data_.begin(); }
+  auto end() { return data_.end(); }
+
   bool empty() const { return data_.empty() || name_.empty(); }
-  void resize(size_t new_size) { data_.resize(new_size); }
   size_t size() const { return data_.size() + name_.size(); }
+
+  void resize(size_t new_size) { data_.resize(new_size); }
   void reserve(size_t new_capacity) { data_.reserve(new_capacity); }
   size_t capacity() const { return data_.capacity(); }
 };
-} // namespace ipb
+}  // namespace ipb
 
-#endif // HOMEWORK_4_H
+#include "ipb_algorithm.h"
+
+#endif  // HOMEWORK_4_H
